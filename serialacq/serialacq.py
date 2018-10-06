ef92b34c57d80e1b5c30a5c4cd29209b45ea351a
Brandon Cummings <cummingb@umich.edu>
2018-10-06 15:16:54 -0400

    change name of find_ports.py

--- serialacq/serialacq.py
+++ serialacq/serialacq.py
@@ -3,7 +3,7 @@ from glob import glob
 import numpy as np
 import serial
 
-from find_devices import find_serial_devices
+from find_ports import find_serial_devices
 from devices.biomonitor import Biomonitor
 
 if __name__ == '__main__':
